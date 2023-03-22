04df322199942619e7009e61d402bc38a9667dc9
Duong Van Toan <toandvph19458@fpt.edu.vn>
2023-03-22 14:49:37 +0700

    implement search ui

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import footer from "./footer.css"
+const Footer = () => {
+  return (
+    <footer>
+        <div className='infooter'>
+            <div>
+                <p className="text-footer">Số ĐKKD 011111111</p>
+                <p className="text-footer">Địa chỉ: Phòng 23, tầng 10, Tòa nhà DPCH</p>
+                <p className="text-footer">số 343 đường Nguyễn Văn Cừ, P. Gia Thụy, Q. Long Biên, Hà Nội</p>
+                <p className="text-footer">Chăm sóc khách hàng: 0243.9388512</p>
+                <p className="text-footer">Mua hàng online: 0246.2909098</p>
+                <p className="text-footer">Email: cskh-speedx@stripe-vn.com</p>
+            </div>
+            <div>
+                <h5 className="text-footer">CHÍNH SÁCH GIAO HÀNG</h5>
+                <p className="text-footer">Địa chỉ: Phòng 23, tầng 10, Tòa nhà DPCH</p>
+                <p className="text-footer">số 343 đường Nguyễn Văn Cừ, P. Gia Thụy, Q. Long Biên, Hà Nội</p>
+                <p className="text-footer">Chăm sóc khách hàng: 0243.9388512</p>
+                <p className="text-footer">Mua hàng online: 0246.2909098</p>
+                <p className="text-footer">Email: cskh-speedx@stripe-vn.com</p>
+            </div>
+            <div>
+                <h5 className="text-footer">CHÍNH SÁCH GIAO HÀNG</h5>
+                <p className="text-footer">Địa chỉ: Phòng 23, tầng 10, Tòa nhà DPCH</p>
+                <p className="text-footer">số 343 đường Nguyễn Văn Cừ, P. Gia Thụy, Q. Long Biên, Hà Nội</p>
+                <p className="text-footer">Chăm sóc khách hàng: 0243.9388512</p>
+                <p className="text-footer">Mua hàng online: 0246.2909098</p>
+                <p className="text-footer">Email: cskh-speedx@stripe-vn.com</p>
+            </div>
+            <div>
+                <h5 className="text-footer">CHÍNH SÁCH GIAO HÀNG</h5>
+                <p className="text-footer">Địa chỉ: Phòng 23, tầng 10, Tòa nhà DPCH</p>
+                <p className="text-footer">số 343 đường Nguyễn Văn Cừ, P. Gia Thụy, Q. Long Biên, Hà Nội</p>
+                <p className="text-footer">Chăm sóc khách hàng: 0243.9388512</p>
+                <p className="text-footer">Mua hàng online: 0246.2909098</p>
+                <p className="text-footer">Email: cskh-speedx@stripe-vn.com</p>
+            </div>
+        </div>
+        <div>
+     
+            <div className='end-footer'>
+                <div><p>© 2022 - Bản quyền SpeedX. Cung cấp bởi Haravan</p></div>
+                <div>
+                    <a href=""><i class="uil uil-twitter"></i></a>
+                    <a href=""><i class="uil uil-instagram-alt"></i></a>
+                    <a href=""><i class="uil uil-facebook"></i></a>
+                    <a href=""><i class="uil uil-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
