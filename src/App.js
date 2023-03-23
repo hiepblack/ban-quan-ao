58e2239c96e25a9f473649df523dc312d1fdaef7
hiepblack <chuongduong0123@gmail.com>
2023-03-23 10:55:09 +0700

    2303chiaylauyout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import "./App.css";
-import Be_Header from "./components/be-header/Be_Header";
-import Header from "./components/header/Header";
-import Slide from "./components/slideshow/Slide";
-import Hot_Collect from "./components/hot-collects/Hot_Collect";
-import New_Product from "./components/newProduct/New_Product";
-import Title from "./components/title/Title";
-import Footer from "./components/footer/Footer";
-import CardProducts from "./components/cardProduct/CardProducts";
+import Client from "./layout/Client.jsx"
 
 function App() {
   return (
     <div className="App">
-      <Be_Header />
-      <Header />
-      <div className="main">
-        <Slide />
-        <Hot_Collect />
-      </div>
-      <Footer />
-      <CardProducts />
+      <Client/>
     </div>
   );
 }
